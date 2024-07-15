d9f49e913b5adfa526cc8b63cb0de1f81f05055a
Mahnoor-Liaquat <mahnoorliaquat6@gmail.com>
2024-07-15 07:41:43 +0500

    day 2 update

---
+++ Day 2/index.js
@@ -0,0 +1,97 @@
+// Day 2: Operators:
+
+// Activity 1 : Arithmetic Operations
+
+// Task 1:
+let add1 = 21;
+let add2 = 2;
+console.log(add1+add2);
+
+// Task 2:
+let sub1 = 21;
+let sub2 = 2;
+console.log(sub1-sub2);
+
+// Task 3:
+let mul1 = 21;
+let mul2 = 2;
+console.log(mul1*mul2);
+
+// Task 4:
+let div1 = 21;
+let div2 = 2;
+console.log(div1/div2);
+
+// Task 5:
+let num1 = 21;
+let num2 = 2;
+const rem = num1%num2;
+console.log(rem);
+
+// Activity 2: Assignments Operators
+
+// Task 6:
+let sum = 21;
+sum +=2;
+console.log(sum)
+
+// Task 7:
+let sub = 21;
+sub -=2;
+console.log(sub)
+
+// Activity 3: Comparison Operators
+
+// Task 8:
+let no1 = 23;
+let no2 = 24;
+console.log(no1<no2);
+console.log(no1>no2);
+
+// Task 9:
+no1 = 34;
+no2 = 23;
+console.log(no1>=no2);
+console.log(no1<=no2);
+
+// Task 10
+let number1 = 21;
+let number2 = "21";
+console.log(number1 == number2);
+console.log(number1 === number2);
+
+// Activity 4: Logical Operators
+
+// Task 11
+let x=3;
+let y = 5;
+
+console.log(x<=y && y>=x);
+
+// Task 12
+let age1 = 21;
+let age2 = 33;
+console.log(age1<=age2 || age1>=age2);
+
+// Task 13
+let myNo =32;
+let yourNo = 22;
+console.log(myNo !== yourNo);
+
+// Activity 5: Ternary Operator
+
+// Task 14
+let number = -4;
+
+let result = number > 0 ? "Number is positive" : number < 0 ? "Number is negative" : "Number is zero";
+console.log(result);
+
+
+
+
+
+
+
+
+
+
